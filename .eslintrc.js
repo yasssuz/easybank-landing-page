7f2754e3971cd08cd3b37a7c28cb815fdda7d339
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-01 23:40:05 -0300

    added new rules and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'prettier',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 0,
+  },
+};
